b29552fc3a37e2af8831e11538e6f5ebac01e57e
juacomiranda <jmiranda@live.cl>
2022-10-27 04:00:59 +0000

    ejercicio 4 termiando

---
+++ src/app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    jls_extract_var = "<h1>Hello!</h1>"
+    return jls_extract_var
+
+
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3245, debug=True)
