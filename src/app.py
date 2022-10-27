22d8e158934cce75f8fe075ffbfae4bd9c4a2c4b
juacomiranda <jmiranda@live.cl>
2022-10-27 13:22:14 +0000

    fin

--- src/app.py
+++ src/app.py
@@ -1,13 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify, request,json
 app = Flask(__name__)
 
+todo = [{   "label": "My first task", "done": False }]
+decoded_object = json.loads(todo)
+
+
 @app.route('/todos', methods=['GET'])
-def hello_world():
-    jls_extract_var = "<h1>Hello!</h1>"
-    return jls_extract_var
+def hello_world ():
+    json_text = jsonify(todo)
+    return json_text
 
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    print("Incoming request with the following body", request_body)
+    return jsonify(todo)
 
-
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todo.remove("My first task")
+    return jsonify(todo)
 
 
 if __name__ == '__main__':
